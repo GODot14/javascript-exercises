215c36afd9718f7c6d03c39518adc97c8ed31f7c
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-12-29 13:09:01 +0100

    Complete the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+function removePunctuation(text) {
+    let punctuation = /[\.,?!\s]/g;
+    let newText = text.replace(punctuation, '');
+    return newText.toLowerCase();
+}
 
+const palindromes = function (string) {
+    string = removePunctuation(string);
+    let reversedString = [];
+    for (let index = 0; index < string.length; index++) {
+        reversedString.unshift(string[index]);
+    }
+
+    return reversedString.join('') === string ? true : false;
 };
 
 // Do not edit below this line
