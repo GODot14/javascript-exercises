6f1a2c4c4e8ce957eed1098b84c0449b5a1e18f5
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-12-29 15:21:42 +0100

    Complete the exercice

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = people.reduce((oldest, currentPerson) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
 
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
+    return oldestPerson;
 };
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+//console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
