950c60d5c37d154bea31ed7e51c617654cc49b86
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-12-29 12:12:29 +0100

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let totalSum = 0;
+	a.forEach(element => {
+    totalSum += element;
+  });
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let totalMulti = 1;
+  a.forEach(element => {
+    totalMulti *= element;
+  })
+  return totalMulti;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1) return 1;
+    let totalFact = 0;
+    for (let n = a - 1; n > 0; n--) {
+      totalFact = a *= n;
+    }
+    return totalFact;
 };
 
 // Do not edit below this line
