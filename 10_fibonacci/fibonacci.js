d6a2c150bb0aa318f8edf944cf1630e51967287a
GODot14 <149940783+GODot14@users.noreply.github.com>
2023-12-29 13:43:49 +0100

    Complete the exercice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+let sequence = [0, 1];
 
+const fibonacci = function(n) {
+    if (n < 0) {
+        return 'OOPS';
+    } else if (n === 0 || n === 1) {
+        return sequence[n];
+    }
+
+    for (let i = 2; i <= n; i++) {
+        sequence[i] = sequence[i - 2] + sequence[i - 1];
+    }
+    return sequence[n];
 };
 
 // Do not edit below this line
